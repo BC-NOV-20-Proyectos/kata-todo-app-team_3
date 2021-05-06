cd6a7f1f40811706cc8fceac8303ad487544e0f0
Diego <ldvera41@gmail.com>
2021-05-06 17:41:05 -0500

    Added active storage to the project
    Active storage was added to the project
    into users_controller was generated new methods (edit, update, show)
    the user now can upload a profile photo
    the avatar param was permited into the user_params
    was added relation has_one_attached into the user model
    _form for user was modificated to user a form_with
    was created 2 new files into views/users (edit/show.html.erb)
    rails active_storage:install generated a new migration
    
    Co-authored-by: jnegrete15 <jnegret15@ucol.mx>

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
-  def index; end
+  def index
+    # @user = User.find(params[:id])
+  end
+
+  def edit
+    
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,33 @@
 # frozen_string_literal: true
 
 class UsersController < Clearance::UsersController
+  before_action :set_user, only: %i[edit update show]
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "user was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+
+  end 
+  
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
-    params.require(:user).permit(:name, :lastname, :email, :password)
+    params.require(:user).permit(:name, :lastname, :email, :password, :avatar)
   end
 end
