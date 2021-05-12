ac6fb2157d511a520b92bebd2949ce28f80712b2
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-05-12 16:44:57 -0500

    Desktop design done

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
+
   def index
-    # @user = User.find(params[:id])
+    @tasks = Task.where(user_id: current_user.id)
   end
 
   def edit
