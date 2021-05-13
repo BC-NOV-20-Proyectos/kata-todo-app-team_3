2bf4486e2278041578c6235da2757f1ec526d112
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-05-13 16:48:10 -0500

    Adding styles to:
    -Edit task page
    -Home taks page
    -Main/index page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
+  before_action :set_task, only: %i[ destroy ]
 
   def index
     @tasks = Task.where(user_id: current_user.id)
@@ -8,6 +9,20 @@ class MainController < ApplicationController
 
   def edit
     
+  end
+
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_task 
+    @task = Task.find(params[:id])
   end
   
 end
