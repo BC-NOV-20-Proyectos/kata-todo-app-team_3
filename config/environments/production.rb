6e794d624a7fcb5985a18730359f94f6ef68f5cd
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-05-20 13:38:23 -0500

    Line duplicated on production.rb deleted

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,6 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
 
   config.assets.check_precompiled_asset = false
-  config.active_storage.service = :azure
   
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -44,7 +43,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :azure
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
