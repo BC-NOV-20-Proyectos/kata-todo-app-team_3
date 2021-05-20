8d24a073a63b8964772cd325c8882ebb4729ea40
Jose Alberto Negrete <jnegrete15@ucol.mx>
2021-05-20 12:37:15 -0500

    login issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def new
+  skip_before_action :verify_authenticity_token
 
-  end
-
-  def edit
-    
-  end
-  
 end
